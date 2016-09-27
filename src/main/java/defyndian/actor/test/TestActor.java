859fe7b226aa946542a408de81d5f05a4002815e
J.Kidd <jkidd@defyndian.co.uk>
2016-09-27 01:01:15 +0100

    Brought up to date with defyndian-core:1.2.0

--- src/main/java/defyndian/actor/test/TestActor.java
+++ src/main/java/defyndian/actor/test/TestActor.java
@@ -1,38 +1,36 @@
 package defyndian.actor.test;
 
+import com.rabbitmq.client.Connection;
+import defyndian.config.ConfigType;
+import defyndian.config.DefyndianConfig;
 import defyndian.core.DefyndianActor;
+import defyndian.datastore.exception.DatastoreCreationException;
 import defyndian.exception.ConfigInitialisationException;
 import defyndian.exception.DefyndianDatabaseException;
 import defyndian.exception.DefyndianMQException;
 import defyndian.messaging.BasicDefyndianMessage;
 import defyndian.messaging.DefyndianMessage;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class TestActor extends DefyndianActor{
 
-	public TestActor(String arg0) throws DefyndianMQException, DefyndianDatabaseException, ConfigInitialisationException {
-		super(arg0);
+	private static final Logger logger = LoggerFactory.getLogger(TestActor.class);
+
+	public TestActor(String arg0, Connection connection, DefyndianConfig config) throws DefyndianMQException, DefyndianDatabaseException, ConfigInitialisationException, DatastoreCreationException {
+		super(arg0, connection, config);
 	}
 
 	protected void handleMessage(BasicDefyndianMessage message) {
 		logger.info("Received: " + message);
 	}
 	
-	public static void main( String...args ){
-
-		DefyndianActor actor = null;
-		try {
-			 actor = new TestActor("Test");
-			 actor.start();
-		} catch (Exception e) {
-			System.err.println("Global Error catcher: " + e);
-			e.printStackTrace();
-		} finally {
-			System.err.println("Shutting down"); 
-			if( actor!=null ){
-				System.err.println("Shutting down");
-				actor.close();
-			}
-		}
+	public static void main( String...args ) throws Exception {
+		final String name = "TestActor";
+		final DefyndianActor actor;
+		final DefyndianConfig config = DefyndianConfig.getConfig(name);
+		actor = new TestActor(name, config.getRabbitMQDetails().getConnectionFactory().newConnection(), config);
+		actor.start();
 	}
 
 }
--- src/test/java/defyndian/actor/test/TestActorTest.java
+++ src/test/java/defyndian/actor/test/TestActorTest.java
@@ -1,17 +1,31 @@
 package defyndian.actor.test;
 
+import defyndian.config.DefyndianConfig;
+import defyndian.core.Consumer;
+import defyndian.core.Publisher;
+import defyndian.messaging.BasicDefyndianMessage;
 import org.junit.Test;
 
 import defyndian.core.DefyndianActor;
 import defyndian.exception.DefyndianDatabaseException;
 import defyndian.exception.DefyndianMQException;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
 
+@RunWith(MockitoJUnitRunner.class)
 public class TestActorTest {
+
+	@Mock private Publisher publisher;
+	@Mock private Consumer consumer;
+	@Mock private DefyndianConfig config;
+
+	@InjectMocks
+	private TestActor target;
 
 	@Test
 	public void testActor() throws Exception{
-		DefyndianActor actor = new TestActor("TESTING");
-		actor.start();
-		actor.s
+		target.handleMessage(new BasicDefyndianMessage(""));
 	}
 }
